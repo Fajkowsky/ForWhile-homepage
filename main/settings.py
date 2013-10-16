dc470f38a4f7ab2ba68f6d05f9f825f7677dc314
Fajkowsky <dawid.fajkowski@skygate.pl>
2013-10-16 17:47:14 +0200

    [settings] added connection to database

--- main/settings.py
+++ main/settings.py
@@ -13,13 +13,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'dqbtn854n5kpv',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'USER': 'crbckeazukswcs',
+        'PASSWORD': 'bGJ4AuewtbCluatx_wN2MBCjK7',
+        'HOST': 'ec2-23-21-133-106.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
